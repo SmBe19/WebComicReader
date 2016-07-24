4b589cc7a05c4958bf6481cb31bf678492c150de
SmBe19 <git@schmidb.ch>
2016-07-24 20:20:14 +0200

    Implement Zoom.

--- app/src/main/java/com/smeanox/apps/webcomicreader/ComicActivity.java
+++ app/src/main/java/com/smeanox/apps/webcomicreader/ComicActivity.java
@@ -1,5 +1,6 @@
 package com.smeanox.apps.webcomicreader;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -11,12 +12,16 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ZoomControls;
 
 import com.smeanox.apps.webcomicreader.providers.ComicProvider;
 
@@ -24,6 +29,8 @@ import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Stack;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ComicActivity extends AppCompatActivity {
 
@@ -35,6 +42,9 @@ public class ComicActivity extends AppCompatActivity {
 	private ComicProvider provider;
 
 	private Stack<Comic> lastComics;
+
+	private Timer zoomControlsTimer;
+	private boolean zoomControlsUsed;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +93,59 @@ public class ComicActivity extends AppCompatActivity {
 			}
 		}
 
+		initZooming();
+
 		updateCurrentComic();
+	}
+
+	protected void initZooming(){
+		final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		final ZoomControls zoomControls = (ZoomControls) findViewById(R.id.imageZoomControls);
+		assert imageView != null && zoomControls != null;
+		imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+		zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				float currentScale = imageView.getScaleX();
+				float newScale = currentScale * 1.25f;
+				imageView.setScaleX(newScale);
+				imageView.setScaleY(newScale);
+
+				zoomControlsUsed = true;
+			}
+		});
+		zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				float currentScale = imageView.getScaleX();
+				float newScale = currentScale / 1.25f;
+				imageView.setScaleX(newScale);
+				imageView.setScaleY(newScale);
+
+				zoomControlsUsed = true;
+			}
+		});
+		zoomControls.setVisibility(View.GONE);
+		imageView.setOnTouchListener(new ImageViewScaleListener(this));
+
+		zoomControlsTimer = new Timer();
+		zoomControlsTimer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				if (zoomControlsUsed) {
+					zoomControlsUsed = false;
+				} else {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							zoomControls.setVisibility(View.GONE);
+						}
+					});
+
+					zoomControlsUsed=true;
+				}
+			}
+		}, 0, getResources().getInteger(R.integer.zoomBarTimeout));
 	}
 
 	@Override
@@ -212,6 +274,9 @@ public class ComicActivity extends AppCompatActivity {
 			comicidView.setText(String.valueOf(currentComic.getId()));
 			altView.setText(currentComic.getAltText());
 		}
+		imageView.scrollTo(0, 0);
+		imageView.setScaleX(1);
+		imageView.setScaleY(1);
 
 		prefs.edit().putInt(PREFS_CURRENT_COMIC, currentComic.getId()).commit();
 	}
@@ -243,6 +308,72 @@ public class ComicActivity extends AppCompatActivity {
 		} else {
 			currentComic = lastComics.pop();
 			updateCurrentComic();
+		}
+	}
+
+	protected static class ImageViewScaleListener extends GestureDetector.SimpleOnGestureListener implements ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
+
+		private ScaleGestureDetector scaleGestureDetector;
+		private GestureDetector gestureDetector;
+		private Activity context;
+
+		public ImageViewScaleListener(Activity context) {
+			this.context = context;
+			scaleGestureDetector = new ScaleGestureDetector(context, this);
+			gestureDetector = new GestureDetector(context, this);
+		}
+
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			float scaleFactor = detector.getScaleFactor();
+
+			System.out.println("onScale " + scaleFactor);
+
+			if(Math.abs(1 - scaleFactor) >= 0.001f) {
+				ImageView imageView = (ImageView) context.findViewById(R.id.imageView);
+				assert imageView != null;
+
+				float currentScale = imageView.getScaleX();
+				float newScale = currentScale * scaleFactor;
+				imageView.setScaleX(newScale);
+				imageView.setScaleY(newScale);
+
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public boolean onScaleBegin(ScaleGestureDetector detector) {
+			return true;
+		}
+
+		@Override
+		public void onScaleEnd(ScaleGestureDetector detector) {
+
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+			ImageView imageView = (ImageView) context.findViewById(R.id.imageView);
+			ZoomControls zoomControls = (ZoomControls) context.findViewById(R.id.imageZoomControls);
+			assert imageView != null && zoomControls != null;
+
+			imageView.scrollBy((int) distanceX, ((int) distanceY));
+
+			return true;
+		}
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			scaleGestureDetector.onTouchEvent(event);
+			gestureDetector.onTouchEvent(event);
+
+			ZoomControls zoomControls = (ZoomControls) context.findViewById(R.id.imageZoomControls);
+			assert zoomControls != null;
+			zoomControls.setVisibility(View.VISIBLE);
+
+			return true;
 		}
 	}
 }
